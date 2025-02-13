837dd97156d4d8d6518bbe39694cf6659a0e968b
Marconi Victor <97639388+Marvicss@users.noreply.github.com>
2025-02-13 22:07:43 +0000

    modify endpoint

--- demo/src/hello-world/HelloWorld.java
+++ demo/src/hello-world/HelloWorld.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String helloWorld(){
         return "Hello World";
     }
